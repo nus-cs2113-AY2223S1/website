c1fd836168b8dec9caf7669fe97c864debcd1dfe
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2022-09-28 09:41:20 +0800

    Site Update. [skip ci]

--- book/gitAndGithub/reviewPRs/index.page-vue-render.js
+++ book/gitAndGithub/reviewPRs/index.page-vue-render.js
@@ -47,6 +47,6 @@ with(this){return _c('li',[_v("Type in an overall comment about the PR, if any. 
 with(this){return _c('li',[_v("Choose "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Approve")]),_v(", "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Comment")]),_v(", or "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Request changes")]),_v(" option as appropriate and click on the "),_c('span',{staticClass:"btn btn-success ps-1 pe-1 pb-0 pt-0"},[_v("Submit review")]),_v(" button.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.0")]),_v(", generated on Wed, 28 Sept 2022, 9:25:30 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.0")]),_v(", generated on Wed, 28 Sept 2022, 9:36:35 GMT+8]")])])])}
 }];
   
